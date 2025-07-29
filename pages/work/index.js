2f719a5ed3c018292ca0a5a5c085134e5b502323
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-07-29 19:03:43 +0530

    Update index.js

--- pages/work/index.js
+++ pages/work/index.js
@@ -7,7 +7,6 @@ import Circles from '../../components/Circles';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../../variants';
 
-
 const Work = () => {
   return (
     <div className='h-full bg-primary/30 py-36 flex items-center'>
@@ -15,14 +14,14 @@ const Work = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col xl:flex-row gap-x-8'>
           {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4
-          xl:mb-0'>
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
             <motion.h2
               variants={fadeIn('up', 0.2)}
               initial='hidden'
               animate='show'
               exit='hidden'
-              className='h2 xl:mt-8'>
+              className='h2 xl:mt-8'
+            >
               My Work <span className='text-accent'>.</span>
             </motion.h2>
             <motion.p
@@ -30,9 +29,10 @@ const Work = () => {
               initial='hidden'
               animate='show'
               exit='hidden'
-              className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+            >
               I offer a range of services to help you achieve your goals, from
-              web development to digital marketing. Let's work together to
+              web development to digital marketing. Let&#39;s work together to
               create something amazing!
             </motion.p>
           </div>
@@ -42,7 +42,8 @@ const Work = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className='w-full xl:max-w-[65%]'>
+            className='w-full xl:max-w-[65%]'
+          >
             {/* slider */}
             <WorkSlider />
           </motion.div>
