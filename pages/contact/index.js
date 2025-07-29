8375d568ede538fdf447efdb7149602f55ac5ff6
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-07-29 19:25:35 +0530

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -43,7 +43,7 @@ const Contact = () => {
             animate='show'
             exit='hidden'
             className='h2 text-center mb-12'>
-            Lets <span className='text-accent'>Connect.</span>
+            Let&apo;s <span className='text-accent'>Connect.</span>
           </motion.h2>
 
           {/* form */}
@@ -94,7 +94,7 @@ const Contact = () => {
               overflow-hidden hover:border-accent group relative'>
               <span className='group-hover:-translate-y-[120%] group-hover:opacity-0
                 transition-all duration-500'>
-                Lets talk
+                Let&apo;s talk
               </span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex
               group-hover:-translate-y-0 group-hover:opacity-100 transition-all
