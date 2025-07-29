314ed8b66d297db96c02da2d58dc6d0537363722
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-07-29 19:47:02 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@
 import Image from "next/image";
 
 // components
-import ParticlesContainer from '../components/ParticlesContainer.js'
-import ProjectBtn from '../components/ProjectsBtn.js'
-import Avatar from '../components/Avatar.js'
+import ParticlesContainer from '../components/ParticlesContainer.js';
+import ProjectBtn from '../components/ProjectsBtn.js';
+import Avatar from '../components/Avatar.js';
 
 // framer motion
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 
 // variants
-import { fadeIn } from '../variants.js'
+import { fadeIn } from '../variants.js';
 
 const Home = () => {
   return (
@@ -24,7 +24,8 @@ const Home = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className="h1">
+            className="h1"
+          >
             Transforming Ideas <br /> Into{' '}
             <span className="text-accent">Stunning Websites</span>
           </motion.h1>
@@ -34,10 +35,11 @@ const Home = () => {
             initial='hidden'
             animate='show'
             exit='hidden'
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">
-            I'm Mahe — a FUll Stack developer, UI/UX designer, and part-time
-            AI Agent tamer. I build sleek, scalable web apps with code that's as clean as
-            my Figma files. Frontend, backend, or a sprinkle of AI magic  I've got it covered
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            I&#39;m Mahe — a Full Stack developer, UI/UX designer, and part-time
+            AI Agent tamer. I build sleek, scalable web apps with code that&#39;s as clean as
+            my Figma files. Frontend, backend, or a sprinkle of AI magic — I&#39;ve got it covered
             with occasional caffeine-powered brilliance.
           </motion.p>
           {/* btn */}
@@ -70,7 +72,8 @@ const Home = () => {
           exit='hidden'
           transition={{ duration: 1, ease: 'easeInOut' }}
           className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32
-        lg:bottom-0 lg:right-[8%]">
+        lg:bottom-0 lg:right-[8%]"
+        >
           <Avatar />
         </motion.div>
       </div>
